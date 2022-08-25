58ab0224d96d37e496205c2f2f84725db6b0991e
Geeru4 <geeru4@icloud.com>
2022-08-25 12:17:01 +0200

    "main"

---
+++ German.py
@@ -0,0 +1,37 @@
+import turtle
+
+mainTurtle = turtle.Turtle()
+
+mainTurtle.pensize(6)
+#квадрат
+mainTurtle.color("green")
+mainTurtle.pendown()
+mainTurtle.right(45)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.penup()
+mainTurtle.goto(-350,0)
+#треугольник 
+mainTurtle.color("red")
+mainTurtle.pendown()
+mainTurtle.left(120)
+mainTurtle.forward(150)
+mainTurtle.left(120)
+mainTurtle.forward(150)
+mainTurtle.left(120)
+mainTurtle.forward(150)
+mainTurtle.penup()
+mainTurtle.goto(-130,-50)
+#круг
+mainTurtle.color("blue")
+mainTurtle.pendown()
+mainTurtle.circle(90)
+
+mainTurtle.write("LOGIKA SCHOOL")
+mainTurtle.hideturtle()
+turtle.exitonclick()
